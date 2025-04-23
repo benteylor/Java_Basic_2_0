4ed7e4b4330003ff000b2c5cfb60737eeb82659a
Grynko Andrey <benteylor@gmail.com>
2025-04-23 12:11:23 +0300

    Первый коммит

---
+++ src/main/java/test_suite/I_First.java
@@ -0,0 +1,12 @@
+package test_suite;
+
+class I_First {
+
+    public static void main (String[] args) {
+System.out.print("fuck \nyou \" \\");
+/* \n - выводит информацию с новой строки
+   \" - выводит кавычку
+   \\ - выводит слэш
+ */
+    }
+}
